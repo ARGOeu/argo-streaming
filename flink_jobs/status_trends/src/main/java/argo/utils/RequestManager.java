07b2083398c2b56ecbcbcc1240bd987acc309f3e
cthermolia-grnet <cthermolia@admin.grnet.gr>
2021-03-02 09:37:47 +0200

    ARGO-2981 Investigate and parse aggregation profiles

--- flink_jobs/status_trends/src/main/java/argo/utils/RequestManager.java
+++ flink_jobs/status_trends/src/main/java/argo/utils/RequestManager.java
@@ -20,6 +20,34 @@ import org.json.simple.parser.ParseException;
  * @author cthermolia
  */
 public class RequestManager {
+ public static JSONObject getAggregationProfileRequest(String baseUri, String key, String proxy) throws IOException, ParseException {
+        JSONObject jsonresult = null;
+        String uri = baseUri + "/aggregation_profiles";
+        Request request = Request.Get(uri);
+        // add request headers
+        request.addHeader("x-api-key", key);
+        request.addHeader(HttpHeaders.ACCEPT, "application/json");
+        if (proxy!=null) {
+            request = request.viaProxy(proxy);
+        }
+        String content = "{}";
+        try {
+            CloseableHttpClient httpClient = HttpClients.custom().build();
+            Executor executor = Executor.newInstance(httpClient);
+            content = executor.execute(request).returnContent().asString();
+
+            JSONParser parser = new JSONParser();
+            jsonresult = (JSONObject) parser.parse(content);
+
+//
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return jsonresult;
+
+    }
 
     public static JSONObject getMetricProfileRequest(String baseUri, String uid, String key, String proxy) throws IOException, ParseException {
         JSONObject jsonresult = null;
