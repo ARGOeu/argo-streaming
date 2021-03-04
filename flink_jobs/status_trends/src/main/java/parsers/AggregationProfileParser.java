9615d67301d4def5b974d5a080bb60d8c3bb1525
cthermolia-grnet <cthermolia@admin.grnet.gr>
2021-03-04 09:20:07 +0200

    ARGO-2983 Parse report configuration from the api

--- flink_jobs/status_trends/src/main/java/parsers/AggregationProfileParser.java
+++ flink_jobs/status_trends/src/main/java/parsers/AggregationProfileParser.java
@@ -20,15 +20,15 @@ import org.json.simple.parser.ParseException;
  */
 public class AggregationProfileParser {
 
-    private static String id;
-    private static String date;
-    private static String name;
-    private static String namespace;
-    private static String endpointGroup;
-    private static String metricOp;
-    private static String profileOp;
-    private static String[] metricProfile = new String[2];
-    private static ArrayList<GroupOps> groups=new ArrayList<>();
+    public static String id;
+    public static String date;
+    public static String name;
+    public static String namespace;
+    public static String endpointGroup;
+    public static String metricOp;
+    public static String profileOp;
+    public static String[] metricProfile = new String[2];
+    public static ArrayList<GroupOps> groups = new ArrayList<>();
 
     public static void loadAggrProfileInfo(String baseUri, String key, String proxy) throws IOException, ParseException {
 
@@ -49,7 +49,7 @@ public class AggregationProfileParser {
                 endpointGroup = (String) dataObject.get("endpoint_group");
                 metricOp = (String) dataObject.get("metric_operation");
                 profileOp = (String) dataObject.get("profile_operation");
-              
+
                 JSONObject metricProfileObject = (JSONObject) dataObject.get("metric_profile");
 
                 metricProfile[0] = (String) metricProfileObject.get("id");
@@ -68,91 +68,32 @@ public class AggregationProfileParser {
                         Iterator<JSONObject> serviceiterator = serviceArray.iterator();
                         HashMap<String, String> services = new HashMap<>();
                         while (serviceiterator.hasNext()) {
-                                JSONObject servObject=(JSONObject)serviceiterator.next();
-                                String servicename = (String) servObject.get("name");
-                                String serviceoperation = (String) servObject.get("operation");
-                                services.put(servicename, serviceoperation);
-                            
+                            JSONObject servObject = (JSONObject) serviceiterator.next();
+                            String servicename = (String) servObject.get("name");
+                            String serviceoperation = (String) servObject.get("operation");
+                            services.put(servicename, serviceoperation);
+
                         }
                         groups.add(new GroupOps(groupname, groupoperation, services));
-                        
+
                     }
                 }
             }
         }
     }
 
-    public static String getId() {
-        return id;
+    private static class GroupOps {
+
+        public String name;
+        public String operation;
+        public HashMap<String, String> services;
+
+        public GroupOps(String name, String operation, HashMap<String, String> services) {
+            this.name = name;
+            this.operation = operation;
+            this.services = services;
+        }
+
     }
 
-    public static void setId(String id) {
-        AggregationProfileParser.id = id;
-    }
-
-    public static String getDate() {
-        return date;
-    }
-
-    public static void setDate(String date) {
-        AggregationProfileParser.date = date;
-    }
-
-    public static String getName() {
-        return name;
-    }
-
-    public static void setName(String name) {
-        AggregationProfileParser.name = name;
-    }
-
-    public static String getNamespace() {
-        return namespace;
-    }
-
-    public static void setNamespace(String namespace) {
-        AggregationProfileParser.namespace = namespace;
-    }
-
-    public static String getEndpointGroup() {
-        return endpointGroup;
-    }
-
-    public static void setEndpointGroup(String endpointGroup) {
-        AggregationProfileParser.endpointGroup = endpointGroup;
-    }
-
-    public static String getMetricOp() {
-        return metricOp;
-    }
-
-    public static void setMetricOp(String metricOp) {
-        AggregationProfileParser.metricOp = metricOp;
-    }
-
-    public static String getProfileOp() {
-        return profileOp;
-    }
-
-    public static void setProfileOp(String profileOp) {
-        AggregationProfileParser.profileOp = profileOp;
-    }
-
-    public static String[] getMetricProfile() {
-        return metricProfile;
-    }
-
-    public static void setMetricProfile(String[] metricProfile) {
-        AggregationProfileParser.metricProfile = metricProfile;
-    }
-
-    public static ArrayList<GroupOps> getGroups() {
-        return groups;
-    }
-
-    public static void setGroups(ArrayList<GroupOps> groups) {
-        AggregationProfileParser.groups = groups;
-    }
-
-    
 }
