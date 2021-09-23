fe0d6e472efabcb5ddf456702b6910d5b1f3541a
Konstantinos Kagkelidis <kaggis@gmail.com>
2021-09-23 03:58:08 +0300

    ARGO-3321 Add url information to low level status metric results

--- flink_jobs/batch_status/src/main/java/argo/batch/MongoStatusOutput.java
+++ flink_jobs/batch_status/src/main/java/argo/batch/MongoStatusOutput.java
@@ -14,6 +14,8 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.UpdateOptions;
 
+import argo.batch.MongoStatusOutput.StatusType;
+
 
 /**
  * MongoOutputFormat for storing status data to mongodb
@@ -99,19 +101,19 @@ public class MongoStatusOutput implements OutputFormat<StatusMetric> {
 	 * Service or Endpoint Group ones.       
 	 */
 	private Document prepDoc(StatusMetric record) {
+		// whatever the type of result might be add always the report and group fields
 		Document doc = new Document("report",this.report)
 				.append("endpoint_group", record.getGroup());
 				
-		
+		// if result is of type status service, append also service information to the results
 		if (this.sType == StatusType.STATUS_SERVICE) {
 			
 			doc.append("service",record.getService());
 		
-		} else if (this.sType == StatusType.STATUS_ENDPOINT) {
-			
-
-			doc.append("service", record.getService())
-			.append("host", record.getHostname());
+		// if the result is not status service (previous check) and not group (current check)
+		// this means it is either status endpoint or status metric so we need
+		// to attach first the extra info data (URL etc...)
+		} else if (this.sType != StatusType.STATUS_ENDPOINT_GROUP) {
 			
 			String info = record.getInfo();
 			if (!info.equalsIgnoreCase("")) {
@@ -127,21 +129,33 @@ public class MongoStatusOutput implements OutputFormat<StatusMetric> {
 				doc.append("info", infoDoc);
 			}
 				
-		} else if (this.sType == StatusType.STATUS_METRIC) {
-		
-			doc.append("service", record.getService())
-			.append("host", record.getHostname())
-			.append("metric", record.getMetric())
-			.append("message", record.getMessage())
-			.append("summary", record.getSummary())
-			.append("time_integer",record.getTimeInt()) 
-			.append("previous_state",record.getPrevState())
-			.append("previous_timestamp", record.getPrevTs())
-			// append the actual data to status metric record in datastore
-			.append("actual_data", record.getActualData())
-			// append original status and threshold rule applied
-			.append("original_status", record.getOgStatus())
-			.append("threshold_rule_applied", record.getRuleApplied());
+			// continuing we use this nested conditional to check if the 
+			// result of type status endpoint or status metric to attach 
+			// in each case the correct fields
+			if (this.sType == StatusType.STATUS_ENDPOINT) {
+				
+
+				doc.append("service", record.getService())
+				.append("host", record.getHostname());
+				
+				
+					
+			} else if (this.sType == StatusType.STATUS_METRIC) {
+			
+				doc.append("service", record.getService())
+				.append("host", record.getHostname())
+				.append("metric", record.getMetric())
+				.append("message", record.getMessage())
+				.append("summary", record.getSummary())
+				.append("time_integer",record.getTimeInt()) 
+				.append("previous_state",record.getPrevState())
+				.append("previous_timestamp", record.getPrevTs())
+				// append the actual data to status metric record in datastore
+				.append("actual_data", record.getActualData())
+				// append original status and threshold rule applied
+				.append("original_status", record.getOgStatus())
+				.append("threshold_rule_applied", record.getRuleApplied());
+			}
 		}
 		
 		
