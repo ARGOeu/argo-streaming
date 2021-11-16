46348b93bf780f8bea7055f0728f136e1e95d771
cthermolia-grnet <cthermolia@admin.grnet.gr>
2021-11-16 12:38:23 +0200

    ARGO-3465 Use DowntimeManager code in StreamStatus job

--- flink_jobs_v2/ProfilesManager/src/main/java/profilesmanager/DowntimeManager.java
+++ flink_jobs_v2/ProfilesManager/src/main/java/profilesmanager/DowntimeManager.java
@@ -1,17 +1,16 @@
 package profilesmanager;
 
-
-
 import org.apache.log4j.Logger;
 
 import argo.avro.Downtime;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import org.apache.avro.Schema;
 
@@ -23,117 +22,122 @@ import org.apache.avro.io.DatumReader;
 import org.apache.commons.io.IOUtils;
 
 /**
- * DowntimeManager manages supplementary downtime information that is needed in computation of a/r scores for endpoint groups
- * Information can be loaded either directly from an avro file or from a list of avro objects
+ * DowntimeManager manages supplementary downtime information that is needed in
+ * computation of a/r scores for endpoint groups Information can be loaded
+ * either directly from an avro file or from a list of avro objects
  */
 public class DowntimeManager {
 
-	/**
-	 * List of Downtime information items
-	 */
-	private ArrayList<DowntimeItem> list;
-	private static final Logger LOG = Logger.getLogger(DowntimeManager.class.getName());
+    /**
+     * List of Downtime information items
+     */
+    private ArrayList<DowntimeItem> list;
+    private static final Logger LOG = Logger.getLogger(DowntimeManager.class.getName());
 
-	/**
-	 * Inner class that holds information about a downtime item which is a actually a 4-tuple (hostname,service,startTime,endTime)
-	 */
-	private class DowntimeItem {
-		String hostname; // name of host
-		String service; // name of service
-		String startTime; // declare start time of downtime
-		String endTime; // declare end time of downtime
+    /**
+     * Inner class that holds information about a downtime item which is a
+     * actually a 4-tuple (hostname,service,startTime,endTime)
+     */
+    private class DowntimeItem {
 
-		
+        String hostname; // name of host
+        String service; // name of service
+        String startTime; // declare start time of downtime
+        String endTime; // declare end time of downtime
 
-		public DowntimeItem(String hostname, String service, String startTime, String endTime) {
-			this.hostname = hostname;
-			this.service = service;
-			this.startTime = startTime;
-			this.endTime = endTime;
+        public DowntimeItem(String hostname, String service, String startTime, String endTime) {
+            this.hostname = hostname;
+            this.service = service;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public String toString(){
+			return String.format("(%s,%s,%s,%s)",hostname,service,startTime,endTime);
 		}
 
-	}
+    }
 
-	public DowntimeManager() {
-		this.list = new ArrayList<DowntimeItem>();
-	}
-	/**
-	 * Inserts new downtime information to Donwtime Manager (hostname,service,startTime,endTime)
-	 */
-	public int insert(String hostname, String service, String startTime, String endTime) {
-		DowntimeItem tmpItem = new DowntimeItem(hostname, service, startTime, endTime);
-		this.list.add(tmpItem);
-		return 0; // All good
-	}
+    public DowntimeManager() {
+        this.list = new ArrayList<DowntimeItem>();
+    }
 
-	/**
-	 * Returns the downtime period (if any) for a specific service endpoint: (hostname,service)
-	 */
-	public ArrayList<String> getPeriod(String hostname, String service) {
+    /**
+     * Inserts new downtime information to Donwtime Manager
+     * (hostname,service,startTime,endTime)
+     */
+    public int insert(String hostname, String service, String startTime, String endTime) {
+        DowntimeItem tmpItem = new DowntimeItem(hostname, service, startTime, endTime);
+        this.list.add(tmpItem);
+        return 0; // All good
+    }
 
-		ArrayList<String> period = new ArrayList<String>();
+    /**
+     * Returns the downtime period (if any) for a specific service endpoint:
+     * (hostname,service)
+     */
+    public ArrayList<String> getPeriod(String hostname, String service) {
 
-		for (DowntimeItem item : this.list) {
+        ArrayList<String> period = new ArrayList<String>();
 
-			if (item.hostname.equals(hostname)) {
-				if (item.service.equals(service)) {
-					period.add(item.startTime);
-					period.add(item.endTime);
-					return period;
-				}
-			}
-		}
+        for (DowntimeItem item : this.list) {
 
-		return null;
+            if (item.hostname.equals(hostname)) {
+                if (item.service.equals(service)) {
+                    period.add(item.startTime);
+                    period.add(item.endTime);
+                    return period;
+                }
+            }
+        }
 
-	}
+        return null;
 
-	/**
-	 * Loads downtime information from an avro file
-	 * <p>
-	 * This method loads downtimes information contained in an .avro file with
-	 * specific avro schema.
-	 * 
-	 * <p>
-	 * The following fields are expected to be found in each avro row:
-	 * <ol>
-	 * <li>start_time: string</li>
-	 * <li>end_time: string</li>
-	 * <li>service: string</li>
-	 * <li>hostname: string</li>
-	 * <li>[optional] tags: hashmap (contains a map of arbitrary key values)
-	 * </li>
-	 * </ol>
-	 * 
-	 * @param avroFile
-	 *            a File object of the avro file that will be opened
-	 * @throws IOException
-	 *             if there is an error during opening of the avro file
-	 */
-	@SuppressWarnings("unchecked")
-	public void loadAvro(File avroFile) throws IOException {
+    }
 
-		// Prepare Avro File Readers
-		DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
-		DataFileReader<GenericRecord> dataFileReader = null;
+    /**
+     * Loads downtime information from an avro file
+     * <p>
+     * This method loads downtimes information contained in an .avro file with
+     * specific avro schema.
+     *
+     * <p>
+     * The following fields are expected to be found in each avro row:
+     * <ol>
+     * <li>start_time: string</li>
+     * <li>end_time: string</li>
+     * <li>service: string</li>
+     * <li>hostname: string</li>
+     * <li>[optional] tags: hashmap (contains a map of arbitrary key values)
+     * </li>
+     * </ol>
+     *
+     * @param avroFile a File object of the avro file that will be opened
+     * @throws IOException if there is an error during opening of the avro file
+     */
+    @SuppressWarnings("unchecked")
+    public void loadAvro(File avroFile) throws IOException {
 
-		try {
-			dataFileReader = new DataFileReader<GenericRecord>(avroFile, datumReader);
+        // Prepare Avro File Readers
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
+        DataFileReader<GenericRecord> dataFileReader = null;
 
-			// Grab avro schema
-			Schema avroSchema = dataFileReader.getSchema();
+        try {
+            dataFileReader = new DataFileReader<GenericRecord>(avroFile, datumReader);
 
-			// Generate 1st level generic record reader (rows)
-			GenericRecord avroRow = new GenericData.Record(avroSchema);
+            // Grab avro schema
+            Schema avroSchema = dataFileReader.getSchema();
 
-			// For all rows in file repeat
-			while (dataFileReader.hasNext()) {
-				// read the row
-				avroRow = dataFileReader.next(avroRow);
-				HashMap<String, String> tagMap = new HashMap<String, String>();
+            // Generate 1st level generic record reader (rows)
+            GenericRecord avroRow = new GenericData.Record(avroSchema);
 
-				// Generate 2nd level generic record reader (tags)
+            // For all rows in file repeat
+            while (dataFileReader.hasNext()) {
+                // read the row
+                avroRow = dataFileReader.next(avroRow);
+                HashMap<String, String> tagMap = new HashMap<String, String>();
 
+                // Generate 2nd level generic record reader (tags)
 //				HashMap<Utf8, String> tags = (HashMap<Utf8, String>) (avroRow.get("tags"));
 //
 //				if (tags != null) {
@@ -141,48 +145,51 @@ public class DowntimeManager {
 //						tagMap.put(item.toString(), String.valueOf(tags.get(item)));
 //					}
 //				}
-
-				// Grab 1st level mandatory fields
-				String hostname = avroRow.get("hostname").toString();
-				String service = avroRow.get("service").toString();
-				String startTime = avroRow.get("start_time").toString();
-				String endTime = avroRow.get("end_time").toString();
+                // Grab 1st level mandatory fields
+                String hostname = avroRow.get("hostname").toString();
+                String service = avroRow.get("service").toString();
+                String startTime = avroRow.get("start_time").toString();
+                String endTime = avroRow.get("end_time").toString();
                 // insert data to list
-				this.insert(hostname, service, startTime, endTime);
+                this.insert(hostname, service, startTime, endTime);
 
-			} // end of avro rows
+            } // end of avro rows
 
-		} catch (IOException ex) {
-			LOG.error("Could not open avro file:" + avroFile.getName());
-			throw ex;
-		} finally {
-			// Close quietly without exceptions the buffered reader
-			IOUtils.closeQuietly(dataFileReader);
-		}
+        } catch (IOException ex) {
+            LOG.error("Could not open avro file:" + avroFile.getName());
+            throw ex;
+        } finally {
+            // Close quietly without exceptions the buffered reader
+            IOUtils.closeQuietly(dataFileReader);
+        }
 
-	}
-	
+    }
 
-	/**
-	 * Loads downtime information from a list of downtime objects
-	 * 
-	 */
-	
-	public void loadFromList( List<Downtime> dnt)  {
-		// IF no downtimes collected return
-		if (dnt==null) return;
-		
-		// For each downtime object in list
-		for (Downtime item : dnt){
-			String hostname = item.getHostname();
-			String service = item.getService();
-			String startTime = item.getStartTime();
-			String endTime = item.getEndTime();
-			// Insert data to list
-			if (hostname != null) this.insert(hostname,service,startTime,endTime);
-		}
-		
+    /**
+     * Loads downtime information from a list of downtime objects
+     *
+     */
+    public void loadFromList(List<Downtime> dnt) {
+        // IF no downtimes collected return
+        if (dnt == null) {
+            return;
+        }
 
-	}
+        // For each downtime object in list
+        for (Downtime item : dnt) {
+            String hostname = item.getHostname();
+            String service = item.getService();
+            String startTime = item.getStartTime();
+            String endTime = item.getEndTime();
+            // Insert data to list
+            if (hostname != null) {
+                this.insert(hostname, service, startTime, endTime);
+            }
+        }
 
+    }
+
+    public String toString() {
+        return Arrays.toString(list.toArray());
+    }
 }
