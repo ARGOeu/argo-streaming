2941ddd5faa1319202cb33d7ed95cce3ed61b18d
cthermolia-grnet <cthermolia@admin.grnet.gr>
2024-06-26 12:22:57 +0300

    clear poms for flink1.17

--- flink_jobs_v3/batch-multi/src/main/java/argo/ar/CalcServiceAR.java
+++ flink_jobs_v3/batch-multi/src/main/java/argo/ar/CalcServiceAR.java
@@ -6,9 +6,9 @@ import java.util.List;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.Collector;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 import argo.avro.GroupEndpoint;
 import argo.avro.GroupGroup;
@@ -24,6 +24,8 @@ import java.util.TreeMap;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import profilesmanager.AggregationProfileManager;
 import profilesmanager.EndpointGroupManager;
 import profilesmanager.GroupGroupManager;
